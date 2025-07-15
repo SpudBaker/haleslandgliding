c0275ef79969855c99b22b8d2ca0cb7fff45bee5
Richard Baker <spudbaker@gmail.com>
2025-07-15 22:59:23 +0100

    LOGIN ALERT

--- src/app/components/auth/auth.ts
+++ src/app/components/auth/auth.ts
@@ -1,15 +1,16 @@
 import { Component, inject } from '@angular/core';
-import { IonButton, IonCol, IonGrid, IonRow, IonInput, IonText, AlertController, AlertOptions, NavController } from '@ionic/angular/standalone';
+import { IonButton, IonCol, IonGrid, IonRow, IonInput, AlertController, AlertOptions, NavController } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth';
 import { DataService } from 'src/app/services/data/data';
 import { catchError, from, map, switchMap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FirebaseError } from '@angular/fire/app';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
-  imports: [CommonModule, FormsModule, IonGrid, IonRow, IonCol, IonInput, IonButton, IonText],
+  imports: [CommonModule, FormsModule, IonGrid, IonRow, IonCol, IonInput, IonButton],
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
   providers: []
@@ -18,7 +19,6 @@ export class AuthComponent {
 
   public inputEmail = '';
   public inputPassword = '';
-  public loginErrMessage: string | undefined;
   private alertController = inject(AlertController);
   private navController = inject(NavController);
 
@@ -33,7 +33,6 @@ export class AuthComponent {
   }
 
   public signIn(){
-    this.loginErrMessage = undefined;
     const opts: AlertOptions = {
       message: "Logging in..."
     }
@@ -48,27 +47,36 @@ export class AuthComponent {
               switchMap(() => this.navController.navigateRoot('user'))
             );
           } else {
-            this.loginErrMessage = 'email does not appear to belong to a member of Mendip Gliding Club';
-            return ac.dismiss();
+            return from(ac.dismiss()).pipe(
+              switchMap(()=> {
+                const optsError: AlertOptions = {message: 'email does not appear to belong to a member of Mendip Gliding Club'};
+                return this.alertController.create(optsError)
+              }),
+              switchMap(ac => ac.present())
+            );
           }
         }),
         catchError(err => {
           const fbe = err as FirebaseError;
-          console.log('signIn catchError', fbe)
-          switch(fbe.code){
-            case 'auth/invalid-email':
-              this.loginErrMessage = 'Invalid email';
-              break;
-            case 'auth/missing-password':
-              this.loginErrMessage = 'Password is required';
-              break;
-            case 'auth/invalid-credential':
-              this.loginErrMessage = 'Incorrect password';
-              break;
-            default:
-              this.loginErrMessage = fbe.message;
-          }
-          return ac.dismiss();
+          return of(ac.dismiss()).pipe(
+            switchMap(() => {
+              let optsError = {message: 'unknown error'};
+              switch(fbe.code){
+                case 'auth/invalid-email':
+                  optsError = {message: 'invalid email'};
+                  break;
+                case 'auth/missing-password':
+                  optsError = {message: 'password is required'};
+                  break;
+                case 'auth/invalid-credential':
+                  optsError = {message: 'invalid credentials'};
+                  break;
+              }
+              return from(this.alertController.create(optsError)).pipe(
+                switchMap(ac => ac.present())
+              )
+            })
+          );
         })
       ))
     ).subscribe();
