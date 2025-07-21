3f26ffe50add5196673c0032e97254adf7ca95c9
Richard Baker <spudbaker@gmail.com>
2025-07-21 22:00:47 +0100

    mobile friendly

--- src/app/components/accounts/accounts.component.ts
+++ src/app/components/accounts/accounts.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { IonItem, IonLabel, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe} from '@angular/common';
 import { DataService } from 'src/app/services/data/data';
 
 @Component({
   selector: 'app-accounts-page',
   templateUrl: 'accounts.component.html',
   styleUrls: ['accounts.component.scss'],
-  imports: [AsyncPipe, IonItem, IonLabel, IonGrid, IonCol, IonRow],
+  imports: [AsyncPipe, CurrencyPipe, IonItem, IonLabel, IonGrid, IonCol, IonRow],
 })
 export class AccountsComponent {
 
