ed7696fe4149b235d176739ad368c3b4fae36d6d
Richard Baker <spudbaker@gmail.com>
2025-08-28 20:29:44 +0100

    download csv

--- src/app/components/giftAidSummary/gift-aid-summary.component.ts
+++ src/app/components/giftAidSummary/gift-aid-summary.component.ts
@@ -1,25 +1,47 @@
 import { Component, inject } from '@angular/core';
-import { IonItem, IonLabel, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
+import { IonButton, IonItem, IonLabel, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
 import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { addIcons } from 'ionicons';
 import { homeOutline } from 'ionicons/icons';
 import { DataService } from 'src/app/services/data/data';
-import { tap } from 'rxjs/operators';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gift-aid-summary-page',
   templateUrl: 'gift-aid-summary.component.html',
   styleUrls: ['gift-aid-summary.component.scss'],
-  imports: [AsyncPipe, CurrencyPipe, IonItem, IonLabel, IonGrid, IonCol, IonRow],
+  imports: [AsyncPipe, CurrencyPipe, IonButton, IonItem, IonLabel, IonGrid, IonCol, IonRow],
 })
 export class GiftAidSummaryComponent {
 
   private dataService = inject(DataService);
   public signingOut = false;
-  public giftAidSummary$ = this.dataService.giftAidSummary$.pipe(tap(data=> console.log(data)))
+  public giftAidSummary$ = this.dataService.giftAidSummary$;
 
   constructor() {
      addIcons({homeOutline});
+  }
+
+  public downloadButtonPress(){
+    this.giftAidSummary$.pipe(
+      first(),
+      map(sum => {
+        const headerRow = '"MemberRef", "MemberNumber", "MemberName", "FromDate", "ToDate", "ClaimDays", "PostCode", "RoundTripMileage", "VehicleType", "PotentialClaimValue"\n';
+        const summaryRow = '"'+sum?.MemberRef+'","'+sum?.MemberNumber+'","'+sum?.MemberName+'","'+sum?.FromDate.toLocaleDateString()+'","'+
+        sum?.ToDate.toLocaleDateString()+'","'+sum?.ClaimDays+'","'+sum?.PostCode+'","'+sum?.RoundTripMileage+'","'+
+        sum?.VehicleType+'","'+sum?.PotentialClaimValue+'"\n';
+        this.download('giftAidSummary', headerRow+summaryRow);
+      })
+    ).subscribe();
+  }
+
+  private download(filename: string, data: any) {
+    let link = document.createElement('a');
+    link.download = filename;
+    let blob = new Blob([data], { type: 'text/csv' });
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
   }
 
 }
